599149fbeb9764a9ffa4ef907498e329b8be2d4d
xaviguasch <xaviguasch@gmail.com>
2021-09-21 20:32:18 +0200

    done first api call and UI data population

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const upper = document.querySelector('.upper')
 const quote = document.querySelector('.quote')
 const lower = document.querySelector('.lower')
 
+const areaCity = document.querySelector('.area__city')
+const timeZone = document.querySelector('#timezone')
+
 const toggleInfo = () => {
   upper.classList.toggle('split')
   quote.classList.toggle('split')
@@ -11,6 +14,28 @@ const toggleInfo = () => {
   btnLess.classList.toggle('show')
   lower.classList.toggle('visible')
 }
+
+const populateWithData = (data) => {
+  let { country_code, region_name, time_zone } = data
+
+  areaCity.textContent = `${region_name}, ${country_code}`
+  timeZone.textContent = time_zone
+}
+
+const getData = async () => {
+  const API = 'https://freegeoip.app/json/'
+
+  const request = await fetch(API)
+
+  if (!request.ok) {
+    throw new Error(request.status)
+  } else {
+    const data = await request.json()
+    return data
+  }
+}
+
+getData().then((data) => populateWithData(data))
 
 btnMore.addEventListener('click', toggleInfo)
 btnLess.addEventListener('click', toggleInfo)
